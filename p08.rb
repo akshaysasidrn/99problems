999c3337f9bef7eea4245d6efebdd72b34dcbbe0
aksh93 <akshaysasidharan93@gmail.com>
2016-08-22 16:07:42 +0530

    8 solutions

--- p08.rb
+++ p08.rb
@@ -1,1 +1,14 @@
-p08.rb
+# Eliminate consecutive duplicates of list elements.
+
+def unique array
+	for i in 0..array.count-1 do
+		if array[i] == array[i+1]
+			array.delete_at i 
+		end
+	end
+	print array
+end
+
+array =  gets.split
+unique array
+puts
