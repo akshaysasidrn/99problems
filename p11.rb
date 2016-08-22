02d50796d09e2ea70c06d0f8855278a14b25c125
aksh93 <akshaysasidharan93@gmail.com>
2016-08-22 17:03:20 +0530

    p11.rb

---
+++ p11.rb
@@ -0,0 +1,28 @@
+# Run-length encoding of a list.
+# Use the result of problem P09 to implement the so-called run-length encoding data compression method. 
+# Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+
+def encode array
+	encoded = []
+	index = 0
+	while index <= array.count-1
+		count = 1
+		sublist = []
+		if array[index] == array[index+1]
+			until array[index] != array[index+1]
+				count = count + 1
+				index = index + 1
+			end
+			sublist << "#{count}#{array[index]}"
+		else
+			encoded << array[index]
+		end
+		index = index + 1
+	encoded << sublist if sublist != []
+	end
+	print encoded
+end
+
+array =  gets.split
+encode array
+puts
